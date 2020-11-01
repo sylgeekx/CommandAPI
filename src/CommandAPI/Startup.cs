abdf87b4e5e427e7e4335856c424072392d931ed
sylgeek <psguillaume@outlook.com>
2020-11-01 13:23:28 +0400

    Add model and reposity

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CommandAPI.Data;
+
 namespace CommandAPI
 {
  public class Startup
@@ -10,6 +12,9 @@ namespace CommandAPI
  {
  
  services.AddControllers();
+
+ services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+
  }
  public void Configure(IApplicationBuilder app,
 IWebHostEnvironment env)
