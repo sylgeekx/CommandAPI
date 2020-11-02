824bfd22a5d949fedaee7198e4750739c11e53f9
sylgeek <psguillaume@outlook.com>
2020-11-02 18:24:48 +0400

    Dbcontext added

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -3,13 +3,22 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CommandAPI.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommandAPI
 {
  public class Startup
  {
+    public IConfiguration Configuration {get;}
+ public Startup(IConfiguration configuration)
+ {
+ Configuration = configuration;
+ } 
  public void ConfigureServices(IServiceCollection services)
  {
+ services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
+ (Configuration.GetConnectionString("PostgreSqlConnection")));
  
  services.AddControllers();
 
